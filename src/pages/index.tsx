c840db4e2430793e5147fb44bdfc3db5d6c50e27
gilsonDias92 <gilsinho.1992@gmail.com>
2021-02-24 23:29:17 -0300

    Criação dos componentes CompletedChallenges e Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -10,6 +12,8 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges/>
+          <Countdown/>
 
         </div>
         <div>
